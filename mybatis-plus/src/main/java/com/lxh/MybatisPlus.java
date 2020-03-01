6dcb648fa13906ebec0cb80ae73fa8440205174e
liuxiaohui <756540872@qq.com>
2020-03-01 21:50:34 +0800

    qwqqqq

--- mybatis-plus/src/main/java/com/lxh/MybatisPlus.java
+++ mybatis-plus/src/main/java/com/lxh/MybatisPlus.java
@@ -7,10 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.lxh.mapper")
 public class MybatisPlus {
     /**
-     *  main方法
+     *  main方法1
      * @param args
      */
     public static void main(String[] args){
         SpringApplication.run(MybatisPlus.class, args);
+    }
+
+    /**
+     *  测试
+     */
+    public void test(){
+
     }
 }
