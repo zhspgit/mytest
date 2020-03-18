35d6aa8cb3b7fdb41288c7267381b3cdbb30fd2b
zhspgit <48002250+zhspgit@users.noreply.github.com>
2020-03-18 22:54:12 +0800

    Update UserController.java

--- mybatis-plus/src/main/java/com/lxh/controller/UserController.java
+++ mybatis-plus/src/main/java/com/lxh/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户111
+ * 用户
  */
 @RestController
 @RequestMapping(value = "/user")
