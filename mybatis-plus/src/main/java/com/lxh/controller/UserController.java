d2f0e841bf54307683af2488f4109a641826bc52
liuxiaohui <756540872@qq.com>
2020-03-18 22:53:00 +0800

    qwer

--- mybatis-plus/src/main/java/com/lxh/controller/UserController.java
+++ mybatis-plus/src/main/java/com/lxh/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.lxh.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户
+ * 用户111
  */
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
     String username = "zhangsan";
     int age =23;
