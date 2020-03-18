53deebdad25a449bef78daa1cd9a9e4dfb7215a8
zhspgit <48002250+zhspgit@users.noreply.github.com>
2020-03-18 22:29:29 +0800

    Update UserController.java

--- mybatis-plus/src/main/java/com/lxh/controller/UserController.java
+++ mybatis-plus/src/main/java/com/lxh/controller/UserController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     int id =1;
-    String username = null;
+    String username = "";
 }
